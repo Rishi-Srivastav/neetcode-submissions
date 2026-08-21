4cc02e2aee51b467d0b90aef520cd4ddf7a2b106
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2026-08-21 17:51:13 +0530

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        // grid[m][n]= grid[m-1][n] +grid[m][n-1];
+        Integer[][] dp = new Integer[m+1][n+1];
+        dp[m][n]=1;
+        // for(int i=0;i<=m;i++){
+        //     dp[i][0]= 1;
+        // }
+        // for(int j=0;j<=n;j++){
+        //     dp[0][j]= 1;
+        // }
+        return calc(0,0,m, n, dp);
+    }
+
+    public int calc(int i, int j, int m, int n, Integer[][] dp){
+       if(i==m-1 && j==n-1)
+        return 1;
+        if(i==m || j==n) return 0;
+        if(dp[i+1][j+1]!=null)
+            return dp[i+1][j+1];
+       int ways = calc(i+1,j,m,n,dp) + calc(i,j+1,m,n,dp); 
+       dp[i+1][j+1]=ways;
+       return dp[i+1][j+1];
+}
+}
