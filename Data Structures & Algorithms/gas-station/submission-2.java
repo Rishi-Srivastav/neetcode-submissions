560c890e9dffce7320d33ac08038f2b19e54cc4d
RISHI SRIVASTAV <59692096+Rishi-Srivastav@users.noreply.github.com>
2026-09-08 22:41:37 +0530

    Add: gas-station - submission-2

---
+++ Data Structures & Algorithms/gas-station/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int n=cost.length;
+        int total_sofar=0;
+        int tank=0;
+        int start=0;
+        for(int i=0;i<n;i++){
+            int net=gas[i]-cost[i];
+            total_sofar+=net;
+            tank+=net;
+            if(tank<0){
+                start=i+1;
+                tank=0;
+            }
+        }
+        return total_sofar>=0?start:-1;
+}
+}
